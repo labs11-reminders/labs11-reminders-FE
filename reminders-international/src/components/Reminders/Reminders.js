0993b6a36f3bc8d688fa5f6e7c52ad2ea375b291
RachelKolk <rach.m.obrien@gmail.com>
2019-04-01 12:48:03 -0400

    commented out additional index in components folder - need to ask why it was there

--- reminders-international/src/components/Reminders/Reminders.js
+++ reminders-international/src/components/Reminders/Reminders.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import SMSForm from './SMSForm/SMSForm.js';
+import SMSForm from './components/SMSForm/SMSForm.js';
 import './Reminders.css';
 
 
