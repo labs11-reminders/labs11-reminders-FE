7a4232581e9122c3af209122008f7678926c02a0
siratl <elishubax@gmail.com>
2019-04-01 20:08:22 -0700

    Created Dashboard component and have it rendering in app.js with route /dashboard

--- reminders-international/src/App.js
+++ reminders-international/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { Route, withRouter, Switch } from 'react-router-dom';
 import { Users, Reminders } from './components';
+import Dashboard from './components/Dashboard/Dashboard';
 import axios from 'axios';
-
-
 
 import Auth from './Auth0/Auth/Auth';
 import Callback from './Auth0/Callback/Callback';
 import Auth0 from './Auth0/Auth0';
 import Home from './Home';
-
 
 import './App.css';
 
@@ -22,50 +20,60 @@ class App extends Component {
       //   name: '',
       //   greeting: ''
       // }],
-  };
-}
-
- auth = new Auth();
-
- handleAuthentication = ({location}) => {
-  if (/access_token|id_token|error/.test(location.hash)) {
-    this.auth.handleAuthentication();
+    };
   }
- }
- 
+
+  auth = new Auth();
+
+  handleAuthentication = ({ location }) => {
+    if (/access_token|id_token|error/.test(location.hash)) {
+      this.auth.handleAuthentication();
+    }
+  };
+
   render() {
-    console.log("App Render", this.state);
-   
-    return  (
-     
+    console.log('App Render', this.state);
+
+    return (
       <div className="App">
-          <Route path="/" render={(props) => <Auth0 auth={this.auth} {...props} />} /> 
-          <Route exact path="/home" render={(props) => <Home auth={this.auth} {...props} />} />
-          <Route exact path="/callback" render={(props) => {
-              this.handleAuthentication(props);
-              return <Callback {...props} /> 
-          }}/>
-          
-          <Route 
-            exact path='/users' 
-            render={props => <Users {...props} 
-            auth={this.auth}
-            users={this.state.users} /> } 
-          />
+        <Route
+          path="/"
+          render={props => <Auth0 auth={this.auth} {...props} />}
+        />
+        <Route
+          exact
+          path="/home"
+          render={props => <Home auth={this.auth} {...props} />}
+        />
+        <Route
+          exact
+          path="/callback"
+          render={props => {
+            this.handleAuthentication(props);
+            return <Callback {...props} />;
+          }}
+        />
 
-          <Route 
-            exact path='/sms-form' 
-            render={props => <Reminders {...props} 
-            users={this.state.reminders} /> } 
-          />
+        <Route
+          exact
+          path="/users"
+          render={props => (
+            <Users {...props} auth={this.auth} users={this.state.users} />
+          )}
+        />
 
+        <Route exact path="/dashboard" component={Dashboard} />
+
+        <Route
+          exact
+          path="/sms-form"
+          render={props => (
+            <Reminders {...props} users={this.state.reminders} />
+          )}
+        />
       </div>
-      
     );
   }
-
 }
 
 export default withRouter(App);
-
-
