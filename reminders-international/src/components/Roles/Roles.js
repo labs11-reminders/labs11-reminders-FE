5d5e6b3870f35bf33ebf892ae0c129b5fb19d61a
Katie-Fizzy <katiecleary@airmaster.lan>
2019-04-16 16:20:46 -0400

    updated roles - desktop + mobile

--- reminders-international/src/components/Roles/Roles.js
+++ reminders-international/src/components/Roles/Roles.js
@@ -1,9 +1,92 @@
 import React, { Component } from 'react';
 import './Roles.css';
-
+import styled from 'styled-components'
 import { Card, Button, Col } from 'reactstrap';
 
 class Roles extends Component {
+  state = {
+    activeRole: null,
+  };
+
+  toggle(role) {
+    if (this.state.activeRole !== role) {
+      this.setState({
+        activeRole: role,
+      });
+    }
+  }
+
+  handleNext = e => {
+    this.props.handleRole(this.state.activeRole);
+  }
+
+  render() {
+    console.log(this.state.activeRole);
+    return (
+      <div className="rolesContainer">
+        <h2>Step 1: What's your role at Reminders International? </h2>
+        <main className="rolesWrapper">
+          <Col sm="6">
+            <Card body className="roleCard">
+              <Button
+                id={this.state.activeRole === '1' ? 'activeBtn' : 'roleBtn'}
+                onClick={() => {
+                  this.toggle('1');
+                }}
+              >
+                Teacher
+              </Button>
+            </Card>
+          </Col>
+
+          <Col sm="6">
+            <Card body className="roleCard">
+              <Button
+                id={this.state.activeRole === '2' ? 'activeBtn' : 'roleBtn'}
+                onClick={() => {
+                  this.toggle('2');
+                }}
+              >
+                Student
+              </Button>
+         
+            </Card>
+          </Col>
+
+          <Col sm="6">
+            <Card body className="roleCard">
+              <Button
+                id={this.state.activeRole === '3' ? 'activeBtn' : 'roleBtn'}
+                onClick={() => {
+                  this.toggle('3');
+                }}
+              >
+                Country Manager
+              </Button>
+            </Card>
+          </Col>
+          <Col sm="6">
+            <Card body className="roleCard">
+              <Button
+                id={this.state.activeRole === '4' ? 'activeBtn' : 'roleBtn'}
+                onClick={() => {
+                  this.toggle('4');
+                }}
+              >
+                Board Member
+              </Button>
+            </Card>
+          </Col>
+        </main>
+        <Button id="size" onClick={this.handleNext}>Next</Button>
+      </div>
+    );
+  }
+}
+
+export default Roles;
+
+{/*class Roles extends Component {
   state = {
     activeRole: null,
   };
@@ -85,4 +168,4 @@ class Roles extends Component {
   }
 }
 
-export default Roles;
+export default Roles;*/}
