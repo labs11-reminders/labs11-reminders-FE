f9cead1de200b3aa3528acf0557ae3cf9871f184
siratl <elishubax@gmail.com>
2019-04-18 16:10:54 -0700

    Added a conditional statement that pops up an alert to prevent a user from proceeding without selecting a role.

--- reminders-international/src/components/Roles/Roles.js
+++ reminders-international/src/components/Roles/Roles.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import './Roles.css';
 // import styled from 'styled-components';
-import { Container, Card, Button, Col, Row, Label } from 'reactstrap';
+import {
+  Container,
+  Card,
+  Button,
+  Col,
+  Row,
+  Label,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'reactstrap';
 // import logo from './logo.svg';
 import student from './student.svg';
 import teacher from './teacher.svg';
@@ -10,9 +21,14 @@ import boardMember from './donation.svg';
 // reminders-international/src/components/Roles/Roles.js
 
 class Roles extends Component {
-  state = {
-    activeRole: null,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRole: null,
+      alertModal: false,
+    };
+    this.toggleAlert = this.toggleAlert.bind(this);
+  }
 
   toggle = role => {
     if (this.state.activeRole !== role) {
@@ -22,8 +38,16 @@ class Roles extends Component {
     }
   };
 
+  toggleAlert() {
+    this.setState({
+      alertModal: !this.state.alertModal,
+    });
+  }
+
   handleNext = e => {
-    this.props.handleRole(this.state.activeRole);
+    this.state.activeRole === null
+      ? this.toggleAlert()
+      : this.props.handleRole(this.state.activeRole);
   };
 
   render() {
@@ -94,6 +118,21 @@ class Roles extends Component {
         <Button className="roleBtn" id="size" onClick={this.handleNext}>
           Next
         </Button>
+
+        <Modal
+          id="alertModalContainer"
+          isOpen={this.state.alertModal}
+          toggle={this.toggleAlert}
+        >
+          <ModalBody id="alertModal">
+            You must select a role to proceed!
+          </ModalBody>
+          <ModalFooter id="alertBtn">
+            <Button color="secondary" onClick={this.toggleAlert}>
+              OK!
+            </Button>
+          </ModalFooter>
+        </Modal>
       </Container>
     );
   }
