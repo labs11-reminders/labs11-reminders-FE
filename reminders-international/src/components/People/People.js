a5c0099c0f2b9c9f39df377e12c1f47cdc4b4cc6
RachelKolk <rach.m.obrien@gmail.com>
2019-04-18 18:18:09 -0400

    Selected group changed to green

--- reminders-international/src/components/People/People.js
+++ reminders-international/src/components/People/People.js
@@ -47,12 +47,10 @@ class People extends Component {
         console.log("**********")
         return (
             <div>
-                <hr></hr>
-                <hr></hr>
+                
                 <h5 className="peopleSearchHeader">Search for a user to add them to your selected group above.</h5>
                 <SearchBar  activeGroup={this.props.activeGroup}/>
-                <hr></hr>
-                <hr></hr>
+                
                 <h4 className="peopleHeader">Users currently in your group</h4>
                 {this.state.group_id ? (
                 <PeopleTable activeGroup={this.state.group_id} activeGroupUsers={this.props.activeGroupUsers}/>
